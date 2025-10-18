387dfa6191254134c3c546be61c2255298769872
Ivan More Flores <ivanyoe79@gmail.com>
2025-10-18 11:19:12 -0300

    fix: add env

--- android/app/src/main/java/com/myfirtsapp/MainApplication.kt
+++ android/app/src/main/java/com/myfirtsapp/MainApplication.kt
@@ -22,7 +22,7 @@ class MainApplication : Application(), ReactApplication {
 
         override fun getJSMainModuleName(): String = "index"
 
-        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+        override fun getUseDeveloperSupport(): Boolean = true
 
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
--- react-native.config.js
+++ react-native.config.js
@@ -4,4 +4,12 @@ module.exports = {
     android: {},
   },
   assets: ['./src/presentation/assets/fonts'],
+  dependencies: {
+    'react-native-config': {
+      platforms: {
+        android: null, // disable Android platform auto linking
+        ios: null, // disable iOS platform auto linking
+      },
+    },
+  },
 };
